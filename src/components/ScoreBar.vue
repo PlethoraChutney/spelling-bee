f3dee0149c4544ca1c9ab6b5c7196121697b7888
Rich <rich@posert.com>
2022-02-10 08:57:37 -0800

    Score bar

--- src/components/ScoreBar.vue
+++ src/components/ScoreBar.vue
@@ -19,15 +19,57 @@ export default {
             }
 
             return currentThreshold;
+        },
+        scoreSvgPaths() {
+            let currentPercentScore = this.score/this.scoreLevels[this.scoreLevels.length - 1] * 300;
+            return [
+                `M 0 25 l ${currentPercentScore} 0`,
+                `M ${currentPercentScore} 25 l 300 0`
+            ]
         }
     }
 }
 </script>
 
 <template>
-    <p>{{currentScoreLevel}}</p>
+    <div id="score-bar">
+        <p>{{currentScoreLevel}}</p>
+        <svg
+        width="300"
+        height="50"
+        >
+            <path class="current-score" fill="transparent"
+            :d="scoreSvgPaths[0]"
+            />
+            <path class="remaining-score" fill="transparent"
+            :d="scoreSvgPaths[1]"
+            />
+        </svg>
+    </div>
 </template>
 
 <style scoped>
+#score-bar {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+}
 
+p {
+    width: max-content;
+    height: 50px;
+}
+
+path {
+    stroke-width: 2px;
+}
+
+.current-score {
+    stroke: #f7da21;
+}
+
+.remaining-score {
+    stroke: #e6e6e6;
+}
 </style>
