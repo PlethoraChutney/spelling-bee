f942cee06a9cfa753675910b28c9af3da861881e
Rich <rich@posert.com>
2022-03-20 13:22:11 -0700

    Friend list color

--- src/components/CoopModal.vue
+++ src/components/CoopModal.vue
@@ -18,8 +18,10 @@
         <div class="h-flex">
             <p>Friends:</p>
             <div class="button"
+            style="padding: 10px;"
             v-for="(friend, index) in friendList"
             :key="index"
+            :class="{'current-friend': friend === this.coopUser}"
             @click="this.coopUser = friend; $emit('getCoopWords', friend)"
             >
             {{friend}}
@@ -88,6 +90,10 @@ div.h-flex {
 div.modal-container {
     width: 90%;
     margin: 10px;
+}
+
+div.button.current-friend {
+    background-color: #f7da21;
 }
 
 </style>
